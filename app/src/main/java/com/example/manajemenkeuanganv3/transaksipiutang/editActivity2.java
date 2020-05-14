fcd4e51e37bda60edba35fd7340ed7c7dd54508a
swordfk <khijabfatkhul@gmail.com>
2020-05-14 10:32:19 +0700

    1.0.0

---
+++ app/src/main/java/com/example/manajemenkeuanganv3/transaksipiutang/editActivity2.java
@@ -0,0 +1,13 @@
+package com.example.manajemenkeuanganv3.transaksipiutang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit2);
+    }
+}
